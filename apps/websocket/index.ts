cb6f826e68d5a672a5d0a17d9f40e484c57bbcef
vipendra <vipendrasinghpatel999@gmail.com>
2025-12-03 16:47:41 +0530

    Added backend, frontend and websocket

---
+++ apps/websocket/index.ts
@@ -0,0 +1,27 @@
+import { prismaClient } from "db/client";
+
+Bun.serve({
+  port: 8081,
+  fetch(req, server) {
+    // upgrade the request to a WebSocket
+    if (server.upgrade(req)) {
+      return; // do not return a Response
+    }
+    return new Response("Upgrade failed", { status: 500 });
+  },
+  websocket: {
+    message(ws, message) {
+      prismaClient.user
+        .create({
+          data: {
+            username: Math.random().toString(),
+            password: Math.random().toString(),
+          },
+        })
+        .then(() => {
+          ws.send(message);
+        })
+        .catch((error) => console.log(error));
+    },
+  },
+});
